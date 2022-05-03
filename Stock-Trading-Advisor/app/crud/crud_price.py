f7378f828be20c10ec5e7d93bce9cb80c1a585b3
Chloe <chloe@dhcp-v048-184.mobile.uci.edu>
2022-05-02 18:04:01 -0700

    change upper case to lower case

--- Stock-Trading-Advisor/app/crud/crud_price.py
+++ Stock-Trading-Advisor/app/crud/crud_price.py
@@ -26,7 +26,9 @@ class CRUDItem(CRUDBase[Price, Any, Any]):
         # purpose: deal with open/close/high/low price
         # add 11 lines start from next line
         filt = filter[0].split()[0]
-        if "Open" in filt or "Close" in filt or "High" in filt or "Low" in filt:
+        # filt2 = filter[0].split()[2]
+
+        if "open" in filt or "close" in filt or "high" in filt or "low" in filt:
             sql = 'SELECT * FROM ( \
             WITH s AS(SELECT "Symbol" FROM overview) \
             SELECT * FROM price \
@@ -35,15 +37,24 @@ class CRUDItem(CRUDBase[Price, Any, Any]):
             WHERE timestamp = {} \
             ORDER BY {} {} \
             ) as x where {} \
-            '.format("'{}'".format(date),  sortby, order, cond_sql.lower())    
-
+            '.format("'{}'".format(date),  sortby, order, cond_sql)    
+        # 0429 JHCHI 
+        # purpose: enable EPS / close, two attributes in different table
+        # change 13 lines start from next line
         else:
-            sql = 'WITH s AS(SELECT "Symbol" FROM overview WHERE {}) \
+            #sql = 'WITH s AS(SELECT "Symbol" FROM overview WHERE {}) \
+            #SELECT * FROM price \
+            #right JOIN s \
+            #ON price.symbol = s."Symbol" \
+            #WHERE timestamp = {} \
+            #ORDER BY {} {}'.format(cond_sql, "'{}'".format(date), sortby, order)
+            sql = 'WITH s AS(SELECT * FROM overview ) \
             SELECT * FROM price \
             right JOIN s \
             ON price.symbol = s."Symbol" \
-            WHERE timestamp = {} \
+            WHERE {} and timestamp = {} \
             ORDER BY {} {}'.format(cond_sql, "'{}'".format(date), sortby, order)
+
         # print(sql)
         cur = db.execute(sql)
         return (
