a4eec48bfb79bfd3a1406a2bbc2039b5c79d6ed0
Anderson Tsai <tiger0922@gmail.com>
2022-05-05 15:08:58 -0700

    Change screener api

--- Stock-Trading-App/src/api/ScreenerAPI.js
+++ Stock-Trading-App/src/api/ScreenerAPI.js
@@ -44,7 +44,7 @@ const ScreenerAPI = {
     BacktestFilter: async (data) => {
         try {
             const res = await axios.post(
-                process.env.REACT_APP_API_ENDPOINT + "/api/v1/screeners/backtest?date=2021-08-10&sortby=volume%20%2A%20close&order=DESC",
+                process.env.REACT_APP_API_ENDPOINT + "/api/v1/screeners/backtest?dateIn=2021-07-13&dateOut=2022-01-09&startsortby=volume%20%2A%20close&order=DESC",
                 data,
                 {
                     'Content-Type': 'application/json'
